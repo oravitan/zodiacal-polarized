c0e29be6e981ceca59d476d186648dbcd4a5c5c0
Or Avitan <oravitan2@gmail.com>
2023-03-31 10:28:47 +0300

    Changed optimization script to gradient descent

--- scripts/generate_mie_models.py
+++ scripts/generate_mie_models.py
@@ -1,8 +1,6 @@
 import numpy as np
-import pandas as pd
-import os
-from tqdm import  tqdm
 import matplotlib.pyplot as plt
+from scipy.optimize import minimize
 
 from zodipy._source_funcs import get_phase_function
 from zodipy.source_params import PHASE_FUNCTION_DIRBE, SPECTRUM_DIRBE
@@ -29,56 +27,34 @@ def bhat_distance(theta, func1, func2):
     return bhat_distance
 
 
+def optimization_cost(x):
+    s_min, s_max, big_gamma, small_gamma = x
+    psm = ParticleSizeModel(s_min=s_min, s_max=s_max, big_gamma=big_gamma, small_gamma=small_gamma)  # create a particle size model
+    mie = MieScatteringModel.train(spectrum, particle_size=psm)  # train a Mie scattering model
+
+    # plot the model
+    dist_from_kesall = []
+    for w in C_w:
+        mueller_125um = mie.get_mueller_matrix(w, theta)  # get the scattering
+        mie_phase_func_125um = mueller_125um[..., 0, 0, 0]
+        cur_dist_from_kesall = distance_from_kelsall(theta, mie_phase_func_125um, c=C_w[w])
+        dist_from_kesall.append(cur_dist_from_kesall)
+    return np.mean(dist_from_kesall)
+
+
 if __name__ == '__main__':
-    spectrum = np.logspace(np.log10(300), np.log10(3500), 20)  # white light wavelength in nm
+    spectrum = np.logspace(np.log10(300), np.log10(3500), 10)  # white light wavelength in nm
     theta = np.linspace(0, np.pi, 100)  # angle in radians
 
     # create a parameter mapping
-    s_min = np.logspace(np.log10(0.0001), np.log10(0.001), 10)  # in um
-    s_max = np.logspace(np.log10(40), np.log10(60), 5)  # in um
-    big_gamma = np.linspace(2, 4, 5)
-    small_gamma = np.linspace(3, 5, 12)
-    BG, SG, SN, SX = np.meshgrid(big_gamma, small_gamma, s_min, s_max)  # create a grid of parameters
-    SN, SX, BG, SG = SN.flatten(), SX.flatten(), BG.flatten(), SG.flatten()  # flatten the grid
+    print('Starting optimization')
+    clbk = lambda x: print(f"Current parameters: {x}")
+    x0 = np.array([0.001, 50, 3.8, 3])
+    x = minimize(optimization_cost, x0, method='Nelder-Mead', tol=1, callback=clbk)
+    print("Finished with x=", x)
 
-    parameter_mapping = pd.DataFrame({'s_min': SN, 's_max': SX, 'big_gamma': BG, 'small_gamma': SG})  # create a dataframe from the grid
-
-    results = []
-    for ind, row in tqdm(parameter_mapping.iterrows(), total=len(parameter_mapping)):
-        # create a Mie scattering model
-        psm = ParticleSizeModel(s_min=row['s_min'], s_max=row['s_max'], big_gamma=row['big_gamma'],
-                                small_gamma=row['small_gamma'])  # create a particle size model
-        mie = MieScatteringModel.train(spectrum, particle_size=psm)  # train a Mie scattering model
-
-        # plot the model
-        dist_from_kesall = {}
-        for w in C_w:
-            mueller_125um = mie.get_mueller_matrix(w, theta)  # get the scattering
-            mie_phase_func_125um = mueller_125um[..., 0, 0, 0]
-            cur_dist_from_kesall = distance_from_kelsall(theta, mie_phase_func_125um, c=C_w[w])
-            dist_from_kesall[w] = cur_dist_from_kesall
-
-        mueller_spec = mie.get_mueller_matrix(np.array((spectrum.min(), spectrum.max())), theta)  # get the scattering
-        mie_phase_func_spec = mueller_spec[..., 0, 0]
-        dist_between_spec = bhat_distance(theta, mie_phase_func_spec[..., 0], mie_phase_func_spec[..., -1])
-
-        row_dict = row.to_dict()
-        for w in dist_from_kesall:
-            row_dict[f'dist_from_kesall_{w}'] = dist_from_kesall[w]
-        row_dict['dist_between_spec'] = dist_between_spec
-        results.append(row_dict)
-    results_df = pd.DataFrame(results)
-    results_df = results_df.assign(dist_from_kesall=results_df[['dist_from_kesall_1250', 'dist_from_kesall_2200', 'dist_from_kesall_3500']].mean(axis=1))
-    results_df.to_csv('outputs/mie_parameter_results.csv', index=False)
-
-    results_df.plot.scatter(x='dist_from_kesall', y='dist_between_spec', grid=True, loglog=True)
-    plt.savefig('outputs/mie_dist_from_kesall.pdf', format='pdf')
-    plt.show()
-
-    best_res = results_df.loc[results_df.dist_from_kesall.argmin()].to_dict()
-    print('Best result: ', best_res)
-
-    psm = ParticleSizeModel(s_min=best_res['s_min'], s_max=best_res['s_max'], big_gamma=best_res['big_gamma'], small_gamma=best_res['small_gamma'])  # create a particle size model
+    s_min, s_max, big_gamma, small_gamma = x.x
+    psm = ParticleSizeModel(s_min=s_min, s_max=s_max, big_gamma=big_gamma, small_gamma=small_gamma)  # create a particle size model
     mie = MieScatteringModel.train(spectrum, particle_size=psm)
     mie_scatt = mie(spectrum, theta)
 
@@ -104,8 +80,7 @@ if __name__ == '__main__':
 
     # Display out model in the White Light spectrum
     spectrum_wl = np.logspace(np.log10(300), np.log10(700), 10)  # white light wavelength in nm
-    psm = ParticleSizeModel(s_min=best_res['s_min'], s_max=best_res['s_max'], big_gamma=best_res['big_gamma'],
-                            small_gamma=best_res['small_gamma'])  # create a particle size model
+    psm = ParticleSizeModel(s_min=s_min, s_max=s_max, big_gamma=big_gamma, small_gamma=small_gamma)  # create a particle size model
     mie_wl = MieScatteringModel.train(spectrum_wl, particle_size=psm)
     mie_wl_scatt = mie(spectrum_wl, theta)
 
