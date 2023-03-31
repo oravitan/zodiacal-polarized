369015ca3203f50b9d282a6cacc9af5b1d53bbf8
Or Avitan <oravitan2@gmail.com>
2023-03-31 15:26:36 +0300

    Organized calibration code into a class

--- scripts/camera_calibration_example.py
+++ scripts/camera_calibration_example.py
@@ -1,25 +1,64 @@
+import os
+import pickle as pkl
 import logging
 import numpy as np
 import matplotlib.pyplot as plt
 import astropy.units as u
-from scipy.optimize import least_squares
-from scipy.ndimage import gaussian_filter
+from functools import partial
 
 # import the necessary modules
+from zodipol.estimation.calibration import Calibration
 from zodipol.utils.argparser import ArgParser
-from zodipol.zodipol import Zodipol, Observation
-from zodipol.visualization.skymap_plots import plot_satellite_image, plot_satellite_image_indices
+from zodipol.zodipol import Zodipol
 
 logging_format = '%(asctime)s - %(levelname)s - %(message)s'
 logging.basicConfig(level=logging.INFO, format=logging_format)
 
 
-def make_img(obs, p, add_noise=False, **kwargs):
-    if add_noise:
-        biref_camera_intensity = zodipol.make_camera_images(obs, p, parser["polarization_angle"], n_realizations=parser["n_realizations"], add_noise=add_noise, **kwargs)
+def get_observations(n_rotations):
+    rotations_file_path = 'saved_models/self_calibration_temp.pkl'
+    rotation_list = np.linspace(0, 360, n_rotations, endpoint=False)
+    if os.path.isfile(rotations_file_path):
+        # saved rotations pickle file exists
+        with open(rotations_file_path, 'rb') as f:
+            obs_rot = pkl.load(f)
+        ind_list = np.linspace(0, len(obs_rot), n_rotations, endpoint=False, dtype=int)
+        rotation_list = np.linspace(0, 360, len(obs_rot), endpoint=False)[ind_list]
+        obs_rot = [obs_rot[ii] for ii in ind_list]
     else:
-        biref_camera_intensity = zodipol.make_camera_images(obs, p, parser["polarization_angle"], n_realizations=1, add_noise=add_noise, **kwargs)
-    return biref_camera_intensity
+        obs_rot = [zodipol.create_observation(theta=parser["direction"][0], phi=parser["direction"][1], roll=t * u.deg,
+                                              lonlat=False, new_isl=parser["new_isl"]) for t in tqdm(rotation_list)]
+        with open(rotations_file_path, 'wb') as f:
+            pkl.dump(obs_rot, f)
+    return obs_rot, rotation_list
+
+def get_initial_parameters(obs, parser, zodipol):
+    delta_val, phi_val = np.pi / 4, np.pi / 6
+    delta = zodipol.imager.get_birefringence_mat(delta_val, 'center', flat=True, inv=True)
+    phi = zodipol.imager.get_birefringence_mat(phi_val, 'linear', flat=True, angle=-np.pi / 4)
+    mueller_truth = zodipol.imager.get_birefringence_mueller_matrix(delta, phi)
+
+    obs_biref = [zodipol.imager.apply_birefringence(o, mueller_truth) for o in obs]
+
+    polarization_angle = parser["polarization_angle"]
+    _, polarization_angle_spatial_diff = np.meshgrid(np.arange(parser["resolution"][0]), np.deg2rad(np.linspace(-10, 10, parser["resolution"][1])), indexing='ij')
+    polarization_angle_real = polarization_angle[None, None :] + polarization_angle_spatial_diff.flatten()[:, None, None]
+
+    polarizance, _ = np.meshgrid(np.linspace(0.8, 0.9, parser["resolution"][0]), np.arange(parser["resolution"][1]),
+                       indexing='ij')
+    polarizance_real = polarizance.reshape((-1, 1, 1))
+
+    obs_orig = [zodipol.make_camera_images(o, polarizance_real, polarization_angle_real, n_realizations=1, add_noise=True) for o in obs_biref]
+    images_orig = np.stack(obs_orig, axis=-1)
+    return images_orig, polarizance_real.reshape((-1)), polarization_angle_spatial_diff.reshape((-1)), mueller_truth
+
+
+def cost_callback(calib: Calibration, p, eta, mueller):
+    mueller_est = calib.zodipol.imager.get_birefringence_mueller_matrix(calib.delta, calib.alpha)
+    p_cost = np.mean((p - calib.p)**2)
+    eta_cost = np.mean((eta - calib.eta)**2)
+    mueller_cost = np.mean((mueller - mueller_est)**2)
+    return p_cost, eta_cost, mueller_cost
 
 
 if __name__ == '__main__':
@@ -28,46 +67,44 @@ if __name__ == '__main__':
     parser = ArgParser()
     zodipol = Zodipol(polarizance=parser["polarizance"], fov=parser["fov"],
                       n_polarization_ang=parser["n_polarization_ang"], parallel=parser["parallel"],
-                      n_freq=parser["n_freq"], planetary=parser["planetry"], isl=parser["isl"],
+                      n_freq=parser["n_freq"], planetary=parser["planetary"], isl=parser["isl"],
                       resolution=parser["resolution"], imager_params=parser["imager_params"])
 
     n_rotations = 12
-    obs = [zodipol.create_observation(theta=parser["direction"][0], phi=parser["direction"][1], roll=t * u.deg, lonlat=False,
-                                     new_isl=parser["new_isl"]) for t in np.linspace(0, 360, n_rotations, endpoint=False)]
+    obs, rotation_list = get_observations(n_rotations)
+    calib = Calibration(obs, zodipol, parser)
 
-    # polarizance = np.clip(np.random.normal(parser["polarizance"], 0.03, (len(obs), 1, 1)), a_min=0, a_max=1)
-    polarizance, _ = np.meshgrid(np.linspace(0.8, 0.9, parser["resolution"][0]), np.arange(parser["resolution"][1]),
-                       indexing='ij')
-    polarizance = polarizance.reshape((len(obs[0]), 1, 1))
-    obs_orig = [zodipol.make_camera_images(o, polarizance, parser["polarization_angle"],
-                                           n_realizations=parser["n_realizations"], add_noise=True) for o in obs]
-    images_orig = np.stack(obs_orig, axis=-1)
+    images_orig, polarizance_real, polarization_angle_real, mueller_truth = get_initial_parameters(obs, parser, zodipol)
+    callback_partial = partial(cost_callback, p=polarizance_real, eta=polarization_angle_real, mueller=mueller_truth)
+    p, eta, delta, alpha, cost, itr_cost = calib.calibrate(images_orig, n_itr=5, mode="all", callback=callback_partial)
 
-    # plot_satellite_image_indices(obs_orig, 4, resolution=parser["resolution"])
+    fig, ax = plt.subplots(4, 1, sharex=True, figsize=(6, 6))
+    for ax_i, c in zip(ax, [cost] + list(zip(*itr_cost))):
+        ax_i.semilogy(c, lw=3)
+        ax_i.grid()
+        ax_i.tick_params(labelsize=14)
+    ax[-1].set_xlabel("Iteration", fontsize=16)
+    ax[0].set_ylabel("Total MSE", fontsize=16)
+    ax[1].set_ylabel("$\hat{P}$ MSE", fontsize=16)
+    ax[2].set_ylabel("$\hat{\eta}$ MSE", fontsize=16)
+    ax[3].set_ylabel("$\hat{M}_{biref}$ MSE", fontsize=16)
+    plt.suptitle("Cost vs. Iteration", fontsize=18)
+    plt.tight_layout()
+    plt.savefig("outputs/calib_cost_vs_iteration.pdf")
+    plt.show()
 
-    p_t = np.linspace(0.6, 1, 20)
-    img = [np.stack([zodipol.make_camera_images(o, p, parser["polarization_angle"], n_realizations=1, add_noise=False
-                                                ) for p in p_t], axis=-1) for o in obs]
-    img_stack = np.stack(img, axis=-2)
-
-    diff_resh = (img_stack - images_orig[..., None]).value.reshape(parser["resolution"] + [parser["n_polarization_ang"], n_rotations, len(p_t)])
-    # gg = gaussian_filter(diff_resh, (5, 5, 0, 0), mode='nearest')
-
-    c = np.nansum((1e23 * diff_resh) ** 2, axis=(-3, -2))
-    p_est = p_t[np.argmin(np.nan_to_num(c, nan=np.inf), axis=-1)]
-    # dd = (delta_m - delta_est) / (delta_m + 1e-10)
-
-    fig, (ax1, ax2) = plt.subplots(1, 2)
-    c1 = ax1.imshow(p_est.reshape(parser["resolution"]))
-    c1.set_clim(0.8, 0.9)
-    plt.colorbar(c1, ax=ax1)
-    ax1.set_title('Estimated Per-Pixel\n Polarizance')
-    c2 = ax2.imshow(polarizance.reshape(parser["resolution"]))
-    c2.set_clim(0.8, 0.9)
-    plt.colorbar(c2, ax=ax2)
-    ax2.set_title('Actual Per-Pixel\n Polarizance')
-    fig.tight_layout()
-    plt.savefig('outputs/polarizance_estimation.pdf')
+    # plot comparison of P,eta estimated vs true
+    fig, ax = plt.subplots(2, 2, figsize=(4, 6), subplot_kw={'xticks': [], 'yticks': []})
+    ax[0, 0].imshow(polarizance_real.reshape((parser["resolution"][0], parser["resolution"][1])), cmap='gray', vmin=0.8, vmax=0.9)
+    ax[0, 0].set_title("$P^{true}$")
+    ax[0, 1].imshow(p.reshape((parser["resolution"][0], parser["resolution"][1])), cmap='gray', vmin=0.8, vmax=0.9)
+    ax[0, 1].set_title("$\hat{P}$")
+    ax[1, 0].imshow(polarization_angle_real.reshape((parser["resolution"][0], parser["resolution"][1])), cmap='gray', vmin=-np.pi / 12, vmax=np.pi / 12)
+    ax[1, 0].set_title("$\eta^{true}$")
+    ax[1, 1].imshow(eta.reshape((parser["resolution"][0], parser["resolution"][1])), cmap='gray', vmin=-np.pi / 12, vmax=np.pi / 12)
+    ax[1, 1].set_title("$\hat{\eta}$")
+    plt.tight_layout()
+    plt.savefig("outputs/calib_p_eta.pdf")
     plt.show()
 
     pass
--- zodipol/estimation/calibration.py
+++ zodipol/estimation/calibration.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 from tqdm import tqdm
+from scipy.optimize import least_squares
+from scipy.sparse import eye
 
 
 class Calibration:
@@ -8,15 +10,11 @@ class Calibration:
         self.obs = obs
         self.zodipol = zodipol
         self.parser = parser
-
-        self.p = None
-        self.eta = None
-        self.delta = None
-        self.alpha = None
+        self.initialize()
 
     def initialize(self):
-        self.p = np.ones(self.parser["resolution"])
-        self.eta = np.zeros(self.parser["resolution"])
+        self.p = np.ones(self.parser["resolution"]).reshape((-1))
+        self.eta = np.zeros(self.parser["resolution"]).reshape((-1))
         self.delta = self.zodipol.imager.get_birefringence_mat(0, 'constant', flat=True)
         self.alpha = self.zodipol.imager.get_birefringence_mat(0, 'constant', flat=True)
 
@@ -26,47 +24,60 @@ class Calibration:
         delta = (delta if delta is not None else self.delta)
         alpha = (alpha if alpha is not None else self.alpha)
 
+        p = p.reshape((-1, 1, 1))
+        eta = eta.reshape((-1, 1, 1)) + self.parser["polarization_angle"][None, None, :]
+        # delta = delta.squeeze()
+        # alpha = alpha.squeeze()
+
         biref_mueller = self.zodipol.imager.get_birefringence_mueller_matrix(delta, alpha)
         biref_obs = self.zodipol.imager.apply_birefringence(o, biref_mueller)
         img_model = self.zodipol.make_camera_images(biref_obs, p, eta, n_realizations=1, add_noise=False)
         return img_model
 
-    def calibrate(self, images_orig, n_itr=10, mode="all", disable=False):
+    def calibrate(self, images_orig, n_itr=5, mode="all", disable=False, callback=None):
         self.initialize()
-        itr_cost = []
-        for ii in range(n_itr):
-            self._calibrate_itr(images_orig, mode=mode, disable=disable)
+        itr_cost = [self.get_mse(images_orig)]
+        itr_callback = []
+        if callback is not None:
+            itr_callback.append(callback(self))
+        for _ in tqdm(range(n_itr), disable=disable):
+            self._calibrate_itr(images_orig, mode=mode)
             itr_cost.append(self.get_mse(images_orig))
+            if callback is not None:
+                itr_callback.append(callback(self))
+        if callback is not None:
+            return self.p, self.eta, self.delta, self.alpha, itr_cost, itr_callback
         return self.p, self.eta, self.delta, self.alpha, itr_cost
 
     def get_mse(self, images_orig):
-        img_model = self.forward_model(self.obs)
-        mse = np.nanmean((img_model - images_orig) ** 2)
+        img_model = np.stack([self.forward_model(o) for o in self.obs], axis=-1)
+        mse = np.nanmean((img_model - images_orig) ** 2).value
         return mse
 
-    def _calibrate_itr(self, images_orig, mode="all", disable=False):
+    def _calibrate_itr(self, images_orig, mode="all"):
         if mode == "all":
-            self.p = self._calibrate_property(images_orig, "p", 0.5, 1, 21, disable=disable)
-            self.eta = self._calibrate_property(images_orig, "eta", -np.pi/8, np.pi/8, 21, disable=disable)
-            self.delta = self._calibrate_property(images_orig, "delta", -np.pi/2, np.pi/2, 21, disable=disable)
-            self.alpha = self._calibrate_property(images_orig, "alpha", -np.pi/2, np.pi/2, 21, disable=disable)
+            self.p = self._calibrate_property(images_orig, "p")
+            self.eta = self._calibrate_property(images_orig, "eta")
+            self.delta = self._calibrate_property(images_orig, "delta")
+            self.alpha = self._calibrate_property(images_orig, "alpha")
         elif mode == "P,eta":
-            self.p = self._calibrate_property(images_orig, "p", 0.5, 1, 21, disable=disable)
-            self.eta = self._calibrate_property(images_orig, "eta", np.deg2rad(-15), np.deg2rad(15), 21,disable=disable)
+            self.p = self._calibrate_property(images_orig, "p")
+            self.eta = self._calibrate_property(images_orig, "eta")
         elif mode == "delta,alpha":
-            self.delta = self._calibrate_property(images_orig, "delta", -np.pi/2, np.pi/2, 21, disable=disable)
-            self.alpha = self._calibrate_property(images_orig, "alpha", -np.pi/2, np.pi/2, 21, disable=disable)
+            self.delta = self._calibrate_property(images_orig, "delta")
+            self.alpha = self._calibrate_property(images_orig, "alpha")
         else:
             raise ValueError("Unknown mode")
 
-    def _calibrate_property(self, images_orig, property_name, min_val, max_val, n_steps, disable=False):
-        p_t = np.linspace(min_val, max_val, n_steps)
-        img = [np.stack([self.forward_model(o, **{property_name: p}) for p in p_t], axis=-1) for o in tqdm(self.obs, disable=disable)]
-        img_stack = np.stack(img, axis=-2)
-        diff_resh = (img_stack - images_orig[..., None]).value.reshape(
-            self.parser["resolution"] + [self.parser["n_polarization_ang"], len(self.obs), len(p_t)])
+    def _calibrate_property(self, images_orig, property_name):
+        def cost_function(x):
+            img = [self.forward_model(o, **{property_name: x}) for o in self.obs]
+            img_stack = np.stack(img, axis=-1)
+            diff_resh = (img_stack - images_orig).value
+            cost = 1e20*(diff_resh**2).sum(axis=(-2, -1))**0.5
+            return cost
 
-        c = np.nansum((1e23 * diff_resh) ** 2, axis=(-3, -2))
-        p_est = p_t[np.argmin(np.nan_to_num(c, nan=np.inf), axis=-1)]
-        return p_est
-
+        jac_sparsity = eye(len(self.p))
+        x0 = getattr(self, property_name).squeeze()
+        p_lsq = least_squares(cost_function, x0=x0, jac_sparsity=jac_sparsity, ftol=1e-3, max_nfev=20, verbose=2)
+        return p_lsq.x.reshape((-1))
