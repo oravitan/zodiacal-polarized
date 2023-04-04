Bug fixed Jacobian

zodipol/imager/imager.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import astropy.units as u
-from astropy.constants import h
+from astropy.constants import h, c
 from scipy.stats import multivariate_normal
 
 from zodipol.zodipol.observation import Observation
@@ -83,12 +83,13 @@ class Imager:
 
         # Calculate the number of electrons per pixel
         if frequency is not None:
-            freq_gradient = np.gradient(frequency)
-            n_electrons = np.einsum('ij...,j->i...', energy, freq_gradient / (h * frequency) * weights)  # Number of electrons per frequency
+            df = np.gradient(frequency)
+            n_electrons = np.einsum('ij...,j->i...', energy, df / (h * frequency) * weights)  # Number of electrons per frequency
         else:
             frequency = wavelength.to(u.THz, equivalencies=u.spectral())
-            wavelength_gradient = -np.gradient(wavelength)  # wavelength is sorted by frequency, so it inverse
-            n_electrons = np.einsum('ij...,j->i...', energy, wavelength_gradient / (h * frequency) * weights)
+            df = np.gradient(frequency)
+            jacobian = - c / (wavelength ** 2)
+            n_electrons = np.einsum('ij...,j->i...', energy, jacobian * df / (h * frequency) * weights)
 
         # n_electrons = np.sum(n_electrons_per_freq, axis=1)  # Number of electrons integral
         n_electrons = n_electrons.si  # Number of electrons in SI units

zodipol/zodipol/zodipol.py
@@ -5,6 +5,7 @@ import healpy as hp
 import transformations as transf
 
 from astropy.time import Time
+from astropy.constants import c
 
 from zodipol.utils.math import ang2vec, vec2ang
 from zodipol.zodipol.observation import Observation
@@ -140,8 +141,8 @@ class Zodipol:
         if self.isl is None:
             return 0
         isl_map = self.isl.resize_skymap(nside)
-        df, dw = np.gradient(self.frequency), -np.gradient(self.wavelength)
-        isl_map = isl_map * (dw / df)[None, ...]
+        jacobian = c / (self.wavelength ** 2)
+        isl_map = isl_map * jacobian[None, ...]
         # isl_map = np.stack([isl_map] * len(self.polarization_angle), axis=-1)
         return isl_map
 
@@ -156,8 +157,8 @@ class Zodipol:
             isl = self.isl
             isl_map = isl.get_ang(theta.to('rad').value, phi.to('rad').value)  # resample
 
-        df, dw = np.gradient(self.frequency), -np.gradient(self.wavelength)
-        isl_map = isl_map * (dw / df)[None, ...]
+        jacobian = c / (self.frequency ** 2)
+        isl_map = isl_map * jacobian[None, ...]
         # isl_map = np.stack([isl_map] * len(self.polarization_angle), axis=-1)
         return isl_map
 