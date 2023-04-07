5558cce220e26e2f51d4f7cfc5b24f8f8ed6c604
Or Avitan <avitan@campus.technion.ac.il>
2023-04-07 14:52:44 +0300

    Added optuna-based optimization script

--- scripts/generate_mie_optuna.py
+++ scripts/generate_mie_optuna.py
@@ -22,7 +22,7 @@ C_w = {1250: C_w[1250]}
 
 def distance_from_kelsall(theta, func, c):
     kelsall_125um = get_phase_function(theta, c)
-    mse_kelsall = np.mean(abs(func - kelsall_125um))  # l1 norm
+    mse_kelsall = np.mean((func - kelsall_125um) ** 2)  # l2 norm
     return mse_kelsall
     # bhat = bhat_distance(theta, func, kelsall_125um)
     # return bhat
@@ -39,7 +39,7 @@ def scattering_dop(mueller):
     wanted_dop = -0.33 * np.sin(theta) ** 5
     dop = mueller[..., 0, 0, 1] / mueller[..., 0, 0, 0]
     # return bhat_distance(theta, abs(wanted_dop), abs(dop))
-    mse_dop = np.mean(abs(abs(wanted_dop) - abs(dop)))  # l1 norm
+    mse_dop = np.mean((abs(wanted_dop) - abs(dop)) ** 2)  # l2 norm
     return mse_dop
 
 def generate_model(x, spectrum):
@@ -53,7 +53,7 @@ def generate_model(x, spectrum):
 
 
 def optimization_cost(x):
-    print(f"{datetime.now().strftime('%H:%M:%S.%f')}: Current parameters: {'[' + ', '.join(x.round(5).astype(str)) + ']'}")
+    # print(f"{datetime.now().strftime('%H:%M:%S.%f')}: Current parameters: {'[' + ', '.join(x.round(5).astype(str)) + ']'}")
     mie = generate_model(x, spectrum)
 
     # plot the model
@@ -72,22 +72,44 @@ def optimization_cost(x):
     min_dist = np.mean(dist_from_kesall)
     regularization = np.mean(dop_distance)
     total_cost = min_dist + regularization_factor * regularization
-    print('mean max DoP: ', np.mean(dop), '+-', np.std(dop))  # aiming for a DoP of 0.2
-    print(f"{datetime.now().strftime('%H:%M:%S.%f')}: Cost results: (min_dist={min_dist}) + {regularization_factor}*(regularization={regularization})")
-    print(f"{datetime.now().strftime('%H:%M:%S.%f')}: Cost results: (total_cost={total_cost})")
-    print('--------------------')
+    # print('mean max DoP: ', np.mean(dop), '+-', np.std(dop))  # aiming for a DoP of 0.2
+    # print(f"{datetime.now().strftime('%H:%M:%S.%f')}: Cost results: (min_dist={min_dist}) + {regularization_factor}*(regularization={regularization})")
+    # print(f"{datetime.now().strftime('%H:%M:%S.%f')}: Cost results: (total_cost={total_cost})")
+    # print('--------------------')
     return total_cost
+
+
+def objective(trial):
+    m1_i = trial.suggest_float("m1_i", 1.0, 7.0)
+    m1_j = trial.suggest_float("m1_j", 0.0, 2.0)
+    m2_i = trial.suggest_float("m2_i", 1.0, 7.0)
+    m2_j = trial.suggest_float("m2_j", 0.0, 2.0)
+    m2_prc = trial.suggest_float("m2_prc", 0.0, 0.5)
+    particle_size_small = trial.suggest_float("particle_size_small", 1e-3, 0.5, log=True)
+    particle_size_big = trial.suggest_float("particle_size_big", 0.5, 10, log=True)
+    particle_size_power = trial.suggest_float("particle_size_power", 1.0, 5.0)
+    x = np.array([m1_i, m1_j, m2_i, m2_j, m2_prc, particle_size_small, particle_size_big, particle_size_power])
+    return optimization_cost(x)
 
 
 if __name__ == '__main__':
     spectrum = np.logspace(np.log10(300), np.log10(1300), 10)  # white light wavelength in nm
     theta = np.linspace(0, np.pi, 100)  # angle in radians
 
-    # create a parameter mapping
-    print('Starting optimization')
-    x0 = np.array([6.11175, 0.08278, 3.07128, 0.00182, 0.20079, 0.10301, 0.75446, 1.74194])
-    x = minimize(optimization_cost, x0, method='Nelder-Mead', tol=0.001, options={'disp': True, 'adaptive': True})
-    print("Finished with x=", x)
+    # optuna.delete_study(study_name='mie_optimization', storage='sqlite:///outputs/mie_optimization.db')
+    study = optuna.create_study(study_name='mie_optimization', storage='sqlite:///outputs/mie_optimization.db',
+                                sampler=optuna.samplers.TPESampler(), pruner=optuna.pruners.HyperbandPruner(),
+                                load_if_exists=True)
+    # study.enqueue_trial({ "m1_i": 6.11175, "m1_j": 0.08278, "m2_i": 3.07128, "m2_j": 0.00182, "m2_prc": 0.20079, "particle_size_small": 0.10301,
+    #         "particle_size_big": 0.75446, "particle_size_power": 1.74194})
+    # study.enqueue_trial({'m1_i': 4.27595706533983, 'm1_j': 0.2170492420819936, 'm2_i': 4.95990327842391, 'm2_j': 0.3462876172196013, 'm2_prc': 0.20948996668721834, 'particle_size_small': 0.33357907982230667, 'particle_size_big': 0.7514542362538205, 'particle_size_power': 2.0995733449355694})
+    study.optimize(objective, n_trials=1000)
+    best_params = study.best_params
+    x = [best_params['m1_i'], best_params['m1_j'], best_params['m2_i'], best_params['m2_j'], best_params['m2_prc'], \
+        best_params['particle_size_small'], best_params['particle_size_big'], best_params['particle_size_power']]
+
+    plot_optimization_history(study)
+    plot_contour(study)
 
     mie = generate_model(x, spectrum)
     mie_scatt = mie(spectrum, theta)
