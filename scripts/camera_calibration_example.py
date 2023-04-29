b79df5a47e4ff676b1839bd6d56ea3d19e526a42
Or Avitan <oravitan2@gmail.com>
2023-04-29 14:51:08 +0300

    Organized all calibration visualization in a dedicated module

--- scripts/camera_calibration_example.py
+++ scripts/camera_calibration_example.py
@@ -1,5 +1,3 @@
-import os
-import pickle as pkl
 import logging
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,6 +8,7 @@ from functools import partial
 from zodipol.estimation.calibration import Calibration
 from zodipol.utils.argparser import ArgParser
 from zodipol.zodipol import Zodipol, get_observations, get_initial_parameters
+from zodipol.visualization.calibration_plots import plot_mueller, plot_cost_itr, plot_deviation_comp, plot_res_comp_plot
 
 logging_format = '%(asctime)s - %(levelname)s - %(message)s'
 logging.basicConfig(level=logging.INFO, format=logging_format)
@@ -22,31 +21,6 @@ def cost_callback(calib: Calibration, p, eta, mueller):
     return p_cost, mueller_cost
 
 
-def plot_mueller(mueller, cbar=False, saveto=None):
-    mueller = mueller[..., :3, :3]
-    fig, ax = plt.subplots(3,3, figsize=(6,6), sharex='col', sharey='row', subplot_kw={'xticks': [], 'yticks': []})
-    for i in range(3):
-        for j in range(3):
-            c = ax[i,j].imshow(mueller[..., i, j].reshape(parser["resolution"]), vmin=mueller.min(), vmax=mueller.max())
-            # ax[i,j].get_xaxis().set_visible(False)
-            # ax[i,j].get_yaxis().set_visible(False)
-    ax[0,0].set_ylabel(0, fontsize=16)
-    ax[1,0].set_ylabel(1, fontsize=16)
-    ax[2,0].set_ylabel(2, fontsize=16)
-    ax[2,0].set_xlabel(0, fontsize=16)
-    ax[2,1].set_xlabel(1, fontsize=16)
-    ax[2,2].set_xlabel(2, fontsize=16)
-    # fig.colorbar(c, ax=ax.ravel().tolist())
-    if cbar:
-        cb = fig.colorbar(c, ax=ax.ravel().tolist())
-        cb.ax.tick_params(labelsize=14)
-    else:
-        plt.tight_layout(w_pad=-15.0, h_pad=1.0)
-    if saveto is not None:
-        plt.savefig(saveto, format='pdf', bbox_inches='tight', transparent="True", pad_inches=0)
-    plt.show()
-
-
 if __name__ == '__main__':
     # set params
     logging.info(f'Started run.')
@@ -57,7 +31,7 @@ if __name__ == '__main__':
                       resolution=parser["resolution"], imager_params=parser["imager_params"])
 
     n_rotations = 20
-    n_itr = 10
+    n_itr = 20
     obs, rotation_list = get_observations(n_rotations, zodipol, parser)
 
     obs, images_orig, polarizance_real, polarization_angle_real, mueller_truth = get_initial_parameters(obs, parser, zodipol)
@@ -67,33 +41,10 @@ if __name__ == '__main__':
     calib = Calibration(obs_comb, zodipol, parser)
     init = {'eta': polarization_angle_real}
     p, eta, biref, cost, itr_cost = calib.calibrate(images_orig, n_itr=n_itr, callback=callback_partial, init=init, normalize_eigs=True)
+    p_cost, mueller_cost = list(zip(*itr_cost))
 
-    fig, ax = plt.subplots(3, 1, sharex=True, figsize=(6, 5))
-    for ax_i, c in zip(ax, [cost] + list(zip(*itr_cost))):
-        ax_i.semilogy(c, lw=3)
-        ax_i.grid()
-        ax_i.tick_params(labelsize=14)
-    ax[-1].set_xlabel("Iteration", fontsize=16)
-    ax[0].set_ylabel("Intensity MSE\n($electrons^2$)", fontsize=16)
-    ax[1].set_ylabel("$\hat{P}$ MSE", fontsize=16)
-    ax[2].set_ylabel("$\hat{B}$ MSE", fontsize=16)
-    # plt.suptitle("Cost vs. Iteration", fontsize=18)
-    plt.tight_layout()
-    plt.savefig("outputs/calib_cost_vs_iteration.pdf", format='pdf', bbox_inches='tight', transparent="True", pad_inches=0)
-    plt.show()
-
-    # plot comparison of P,eta estimated vs true
-    fig, ax = plt.subplots(1, 2, figsize=(5, 3), subplot_kw={'xticks': [], 'yticks': []})
-    c1 = ax[0].imshow(polarizance_real[:, 0].reshape((parser["resolution"][0], parser["resolution"][1])), vmin=0.7, vmax=1)
-    ax[0].set_title("$P^{true}$")
-    plt.colorbar(c1, ax=ax[0])
-    c2 = ax[1].imshow(p[:, 0].reshape((parser["resolution"][0], parser["resolution"][1])), vmin=0.7, vmax=1)
-    ax[1].set_title("$\hat{P}$")
-    plt.colorbar(c2, ax=ax[1])
-    plt.savefig("outputs/calib_p_eta.pdf", format='pdf', bbox_inches='tight', transparent="True", pad_inches=0)
-    plt.show()
-
-    plot_mueller(biref, cbar=True, saveto="outputs/calib_mueller_estimation.pdf")
+    plot_cost_itr(cost, p_cost, mueller_cost, saveto="outputs/calib_cost_vs_iteration.pdf")
+    plot_deviation_comp(parser, polarizance_real[:, 0], p[:, 0], saveto="outputs/calib_mueller_estimation.pdf")
 
     A_gamma = zodipol.imager.get_A_gamma(zodipol.frequency, zodipol.get_imager_response())
 
@@ -101,7 +52,7 @@ if __name__ == '__main__':
     n_rotations_list = [4, 6, 10, 14, 18, 22, 26, 30]
     res_cost = []
     for n_rotations in n_rotations_list:
-        obs, rotation_list = get_observations(n_rotations)
+        obs, rotation_list = get_observations(n_rotations, zodipol, parser)
         obs, images_orig, polarizance_real, polarization_angle_real, mueller_truth = get_initial_parameters(obs, parser,
                                                                                                             zodipol)
         obs_comb = zodipol.combine_observations(obs, polarizance=polarizance_real.squeeze(),
@@ -117,23 +68,7 @@ if __name__ == '__main__':
         mean_num_electrons = np.mean((images_orig / A_gamma).to('').value)
         res_cost.append((clbk_itr[-1] / mean_num_electrons, p_cost[-1], mueller_cost[-1]))
     rot_intensity_mse, rot_p_mse, rot_biref_mse = list(zip(*res_cost))
-
-    fig, ax = plt.subplots(1, 1, figsize=(6, 3))
-    ax.semilogy(n_rotations_list, rot_p_mse, lw=2, c='b')
-    ax2 = ax.twinx()
-    ax2.semilogy(n_rotations_list, rot_biref_mse, lw=2, c='r')
-    ax.grid()
-    ax.set_xlabel("Number of observations", fontsize=16)
-    ax.set_ylabel("$P$ MSE", fontsize=16, c='b')
-    ax2.set_ylabel("${\\bf B}$ MSE", fontsize=16, c='r')
-    ax.tick_params(axis='x', labelsize=16)
-    ax.tick_params(axis='y', colors='b', labelsize=16)
-    ax2.tick_params(axis='y', colors='r', labelsize=16)
-    fig.tight_layout()
-    plt.savefig("outputs/calib_mse_n_rotations.pdf", format='pdf', bbox_inches='tight', transparent="True",
-                pad_inches=0)
-    plt.show()
-
+    plot_res_comp_plot(n_rotations_list, rot_p_mse, rot_biref_mse, saveto="outputs/calib_mse_n_rotations.pdf", xlabel="Number of observations")
 
     # now estimate how well we calibration based on exposure time
     n_rotations = 20
@@ -142,7 +77,7 @@ if __name__ == '__main__':
     res_cost = []
     for exposure_time in exposure_time_list:
         zodipol.imager.exposure_time = exposure_time * u.s
-        obs, rotation_list = get_observations(n_rotations)
+        obs, rotation_list = get_observations(n_rotations, zodipol, parser)
         obs, images_orig, polarizance_real, polarization_angle_real, mueller_truth = get_initial_parameters(obs, parser, zodipol)
         obs_comb = zodipol.combine_observations(obs, polarizance=polarizance_real.squeeze(), polarization_angle=polarization_angle_real)
         callback_partial = partial(cost_callback, p=polarizance_real, eta=polarization_angle_real, mueller=mueller_truth)
@@ -154,46 +89,5 @@ if __name__ == '__main__':
         mean_num_electrons = np.mean((images_orig / A_gamma).to('').value)
         res_cost.append((clbk_itr[-1]/mean_num_electrons, p_cost[-1], mueller_cost[-1]))
     ex_intensity_mse, ex_p_mse, ex_biref_mse = list(zip(*res_cost))
+    plot_res_comp_plot(exposure_time_list, ex_p_mse, ex_biref_mse, saveto="outputs/calib_mse_exposure_time.pdf", xlabel="$\Delta t \;(s)$")
 
-    fig, ax = plt.subplots(1, 1, figsize=(6, 3))
-    ax.loglog(exposure_time_list, ex_p_mse, lw=2, c='b')
-    ax2 = ax.twinx()
-    ax2.loglog(exposure_time_list, ex_biref_mse, lw=2, c='r')
-    ax.grid()
-    ax.set_xlabel("$\Delta t \;(s)$", fontsize=16)
-    ax.set_ylabel("$P$ MSE", fontsize=16, c='b')
-    ax2.set_ylabel("${\\bf B}$ MSE", fontsize=16, c='r')
-    ax.tick_params(axis='x', labelsize=16)
-    ax.tick_params(axis='y', colors='b', labelsize=16)
-    ax2.tick_params(axis='y', colors='r', labelsize=16)
-    fig.tight_layout()
-    plt.savefig("outputs/calib_mse_exposure_time.pdf", format='pdf', bbox_inches='tight', transparent="True", pad_inches=0)
-    plt.show()
-
-    fig, (ax, ax2) = plt.subplots(2,1, figsize=(6, 6))
-    ax.semilogy(n_rotations_list, rot_p_mse, lw=2, c='b')
-    ax_d = ax.twinx()
-    ax_d.semilogy(n_rotations_list, rot_biref_mse, lw=2, c='r')
-    ax.grid()
-    ax.set_xlabel("Number of observations", fontsize=16)
-    ax.set_ylabel("$P$ MSE", fontsize=16, c='b')
-    ax_d.set_ylabel("${\\bf B}$ MSE", fontsize=16, c='r')
-    ax.tick_params(axis='x', labelsize=16)
-    ax.tick_params(axis='y', colors='b', labelsize=16)
-    ax_d.tick_params(axis='y', colors='r', labelsize=16)
-    ax_d.yaxis.set_tick_params(which='major', right=False, left=False)
-    plt.setp(ax_d.get_yminorticklabels(), visible=False)
-
-    ax2.semilogy(exposure_time_list, ex_p_mse, lw=2, c='b')
-    ax_d2 = ax2.twinx()
-    ax_d2.semilogy(exposure_time_list, ex_biref_mse, lw=2, c='r')
-    ax2.grid()
-    ax2.set_xlabel("$\Delta t \;(s)$", fontsize=16)
-    ax2.set_ylabel("$P$ MSE", fontsize=16, c='b')
-    ax_d2.set_ylabel("${\\bf B}$ MSE", fontsize=16, c='r')
-    ax2.tick_params(axis='x', labelsize=16)
-    ax2.tick_params(axis='y', colors='b', labelsize=16)
-    ax_d2.tick_params(axis='y', colors='r', labelsize=16)
-    fig.tight_layout()
-    plt.savefig("outputs/calib_mse_exposure_nobs.pdf", format='pdf', bbox_inches='tight', transparent="True", pad_inches=0)
-    plt.show()
--- scripts/self_calibration.py
+++ scripts/self_calibration.py
@@ -1,8 +1,6 @@
 """
 This script performs self-calibration on a Zodipol object.
 """
-import os
-import pickle as pkl
 import numpy as np
 import matplotlib.pyplot as plt
 import astropy.units as u
@@ -12,6 +10,7 @@ from functools import partial
 from zodipol.estimation.self_calibration import SelfCalibration
 from zodipol.utils.argparser import ArgParser
 from zodipol.zodipol import Zodipol, get_observations, get_initial_parameters
+from zodipol.visualization.calibration_plots import plot_deviation_comp, plot_mueller, plot_cost_itr
 
 
 def perform_estimation(zodipol, parser, rotation_list, images_res_flat, polarizance_real, polarization_angle_real, mueller_truth, n_itr=10):
@@ -31,102 +30,15 @@ def cost_callback(calib: SelfCalibration, p, eta, mueller):
     return p_cost, mueller_cost
 
 
-def plot_deviation_comp(parser, polarizance_real, polarizance_est_reshape, saveto=None, set_colors=False):
-    pol_mean_deviation = polarizance_real
-    pol_est_mean_deviation = polarizance_est_reshape
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4))
-    p1, p2 = pol_mean_deviation.squeeze(), pol_est_mean_deviation
-
-    c1 = ax1.imshow(p1.reshape(parser["resolution"]))
-    c2 = ax2.imshow(p2.reshape(parser["resolution"]))
-    cbar1 = fig.colorbar(c1, ax=ax1); cbar1.ax.tick_params(labelsize=14)
-    cbar2 = fig.colorbar(c2, ax=ax2); cbar2.ax.tick_params(labelsize=14)
-    ax1.set_title('True $P$', fontsize=18); ax1.set_axis_off()
-    ax2.set_title('$\hat{P}$', fontsize=18); ax2.set_axis_off()
-    fig.tight_layout()
-    if saveto is not None:
-        plt.savefig(saveto, format='pdf', bbox_inches='tight', transparent="True", pad_inches=0)
-    plt.show()
-
-def plot_cost(n_rotation_list, polariz_bias_l, pol_ang_bias_l, saveto=None, cost_type='MSE', xlabel='Number of observations'):
-    fig, ax = plt.subplots(1, 1)
-    ax.scatter(n_rotation_list, polariz_bias_l)
-    plt.grid()
-    ax2 = ax.twinx()
-    ax2.scatter(n_rotation_list, pol_ang_bias_l, color='r')
-    ax.set_xlabel(xlabel, fontsize=16)
-    ax.set_ylabel(f'Polarizance {cost_type}', color='C0', fontsize=16)
-    ax2.set_ylabel(f'Polarization Angle {cost_type}', color='red', fontsize=16)
-    ax.tick_params(axis='y', colors='C0', labelsize=14)
-    ax2.tick_params(axis='y', colors='red', labelsize=14)
-    ax.tick_params(axis='x', labelsize=14)
-    plt.title(f'{cost_type} of Polarizance and\nPolarization Angle Deviations Estimation', fontsize=18)
-    fig.tight_layout()
-    if saveto is not None:
-        plt.savefig(saveto, format='pdf', bbox_inches='tight', transparent="True", pad_inches=0)
-    plt.show()
-
-def plot_mueller(mueller, parser, cbar=False, saveto=None, vmin=None, vmax=None):
-    if vmin is None:
-        vmin = np.nanmin(mueller)
-    if vmax is None:
-        vmax = np.nanmax(mueller)
-    mueller = mueller[..., :3, :3]
-    fig, ax = plt.subplots(3,3, figsize=(6,6), sharex='col', sharey='row', subplot_kw={'xticks': [], 'yticks': []})
-    for i in range(3):
-        for j in range(3):
-            c = ax[i,j].imshow(mueller[..., i, j].reshape(parser["resolution"]), vmin=vmin, vmax=vmax)
-            # ax[i,j].get_xaxis().set_visible(False)
-            # ax[i,j].get_yaxis().set_visible(False)
-    ax[0,0].set_ylabel(0, fontsize=16)
-    ax[1,0].set_ylabel(1, fontsize=16)
-    ax[2,0].set_ylabel(2, fontsize=16)
-    ax[2,0].set_xlabel(0, fontsize=16)
-    ax[2,1].set_xlabel(1, fontsize=16)
-    ax[2,2].set_xlabel(2, fontsize=16)
-    # fig.colorbar(c, ax=ax.ravel().tolist())
-    if cbar:
-        cb = fig.colorbar(c, ax=ax.ravel().tolist())
-        cb.ax.tick_params(labelsize=14)
-    else:
-        plt.tight_layout(w_pad=-15.0, h_pad=1.0)
-    if saveto is not None:
-        plt.savefig(saveto, format='pdf', bbox_inches='tight', transparent="True", pad_inches=0)
-    plt.show()
-
-
-def plot_cost_itr(cost_itr, p_cost, mueller_cost, saveto=None):
-    fig, ax = plt.subplots(3, 1, figsize=(6, 5), sharex=True)
-    ax[0].plot(cost_itr, lw=3)
-    ax[0].set_ylabel('Intensity MSE\n($electrons^2$)', fontsize=16)
-    ax[0].tick_params(labelsize=16)
-    ax[0].grid()
-
-    ax[1].plot(p_cost, lw=3)
-    ax[1].set_ylabel('$\hat{P}$ MSE', fontsize=16)
-    ax[1].tick_params(labelsize=16)
-    ax[1].grid()
-
-    ax[2].plot(mueller_cost, lw=3)
-    ax[2].set_ylabel('$\hat{B}$ MSE', fontsize=16)
-    ax[2].tick_params(labelsize=16)
-    ax[2].grid()
-    ax[2].set_xlabel('Iteration number', fontsize=16)
-    fig.tight_layout()
-    if saveto is not None:
-        plt.savefig(saveto, format='pdf', bbox_inches='tight', transparent="True", pad_inches=0)
-    plt.show()
-
-
 def main():
     parser = ArgParser()
     zodipol = Zodipol(polarizance=parser["polarizance"], fov=parser["fov"],
                       n_polarization_ang=parser["n_polarization_ang"], parallel=parser["parallel"],
                       n_freq=parser["n_freq"], planetary=parser["planetary"], isl=parser["isl"],
                       resolution=parser["resolution"], imager_params=parser["imager_params"])
-    n_itr = 20
 
     # generate observations
+    n_itr = 20
     n_rotations = 20
     obs_truth, rotation_list = get_observations(n_rotations, zodipol, parser)
     obs_truth, images_res, polarizance_real, polarization_angle_real, mueller_truth = get_initial_parameters(obs_truth, parser, zodipol)
