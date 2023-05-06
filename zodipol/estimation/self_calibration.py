bug fix image rotation

zodipol/estimation/self_calibration.py
@@ -26,16 +26,16 @@ class SelfCalibration(BaseCalibration):
         self.nan_mask = self.get_nan_mask(images_res_flat)
         self.initialize()
 
-    def _calibrate_itr(self, images):
+    def _calibrate_itr(self, images, **kwargs):
         self.obs = self.estimate_observations(images)
         self.estimate_polarizance(images)
         self.estimate_birefringence(images)
 
-    def estimate_polarizance(self, images):
+    def estimate_polarizance(self, images, **kwargs):
         super().estimate_polarizance(images)
         self.p = self.p - np.nanmax(self.p[~self.nan_mask]) + 1
 
-    def estimate_birefringence(self, images, kernel_size: int = None, normalize_eigs: bool = False):
+    def estimate_birefringence(self, images, kernel_size: int = None, normalize_eigs: bool = False, **kwargs):
         super().estimate_birefringence(images, kernel_size=kernel_size, normalize_eigs=normalize_eigs)
 
     def get_nan_mask(self, images):
@@ -51,6 +51,7 @@ class SelfCalibration(BaseCalibration):
         Get the properties of the calibration.
         """
         images = np.stack([self.forward_model(o) for o in self.obs], axis=-1)
+        images[self.nan_mask, ...] = np.nan
         res = [images]
         for ii in [self.p, self.eta, self.biref]:
             ii_copy = ii.copy()

zodipol/utils/math.py
@@ -98,9 +98,9 @@ def get_rotated_image(zodipol, parser, images, rotation_to, fill_value=0):
         vec_from = ang2vec(theta_from, phi_from)
         vec_to = ang2vec(theta_to, phi_to)
 
-        dx = (vec_from[:, 0].max() - vec_from[:, 0].min()) / parser["resolution"][1]
+        dx = (vec_from[:, 2].max() - vec_from[:, 2].min()) / parser["resolution"][1]
         dy = (vec_from[:, 1].max() - vec_from[:, 1].min()) / parser["resolution"][0]
-        x_ind = np.round((vec_to[:, 0] - vec_from[:, 0].min()) / dx).astype(int)
+        x_ind = np.round((vec_from[:, 2].max() - vec_to[:, 2]) / dx).astype(int)
         y_ind = np.round((vec_to[:, 1] - vec_from[:, 1].min()) / dy).astype(int)
         index_mask = (x_ind >= 0) & (x_ind < parser["resolution"][1]) & (y_ind >= 0) & (y_ind < parser["resolution"][0])
         return x_ind, y_ind, index_mask