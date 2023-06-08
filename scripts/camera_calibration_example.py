Fixed B/P ambiguity by eigenvalue normalization

scripts/camera_calibration_example.py
@@ -33,7 +33,7 @@ def calibration(n_rotations: int, n_itr: int, zodipol: Zodipol, parser: ArgParse
     calib = Calibration(obs_comb, zodipol, parser)
     init = {'eta': polarization_angle_real, **initialization}
     p, eta, biref, cost, itr_cost = calib.calibrate(images_orig, n_itr=n_itr, callback=callback_partial, init=init, disable=disable,
-                                                    normalize_eigs=True, kernel_size=5)  # , normalize_eigs=True
+                                                    normalize_eigs=True)  # , normalize_eigs=True
     est_images = np.stack([calib.forward_model(o) for o in obs_comb], axis=-1)
     p_cost, mueller_cost, p_std, mueller_std, p_mad, mueller_mad = list(zip(*itr_cost))
     est_values = {'p': p, 'eta': eta, 'biref': biref, 'images': est_images}
@@ -99,7 +99,7 @@ def plot_calibration_exp(n_rotations=30, n_itr=10, exposure_time_list=None):
 if __name__ == '__main__':
     # set params
     logging.info(f'Started run.')
-    visualize_calibration(n_rotations=10, n_itr=30)
+    visualize_calibration(n_rotations=30, n_itr=10)
     plot_calibration_nbos(n_itr=30, n_rotations_list=None)
     plot_calibration_exp(n_rotations=30, n_itr=30, exposure_time_list=None)
     pass

scripts/self_calibration.py
@@ -40,10 +40,11 @@ def run_self_calibration(n_rotations, n_itr, zodipol, parser, disable=False, dir
     images_res_flat = images_res.reshape((np.prod(parser["resolution"]), parser["n_polarization_ang"], n_rotations))
     images_res_flat = zodipol.post_process_images(images_res_flat)
     initialization = get_initialization(polarizance_real, mueller_truth)
+    star_pixels = np.stack([o.star_pixels for o in obs_truth], axis=-1)
     cost_itr, est_values, clbk_itr = self_calibrate(zodipol, parser, rotation_list, images_res_flat,
                                                     polarizance_real, polarization_angle_real,
                                                     mueller_truth, n_itr=n_itr, disable=disable, max_p=np.max(polarizance_real),
-                                                    initialization=initialization, **kwargs)
+                                                    initialization=initialization, star_pixels=star_pixels, **kwargs)
     true_values = {'images': images_res_flat, 'p': polarizance_real, 'eta': polarization_angle_real, 'biref': mueller_truth}
     return cost_itr, est_values, true_values, clbk_itr
 
@@ -76,6 +77,7 @@ def main_show_cost(n_rotations=10, n_itr=10):
     plot_deviation_comp(parser, true_values["p"][..., 0], est_values['p'][..., 0], set_colors=True,
                         saveto='outputs/self_calibration_polarizance_est.pdf')
     plot_mueller(est_values['biref'], parser, cbar=True, saveto='outputs/self_calib_birefringence_est.pdf')
+    pass
 
 
 def main_plot_n_obs(n_itr=10, n_rotations_list=None):
@@ -152,7 +154,7 @@ def main_plot_uncertainty(n_rotations=10, n_itr=10, direction_error_list=None):
 
 
 def main():
-    main_show_cost(n_rotations=30, n_itr=20)
+    main_show_cost(n_rotations=30, n_itr=10)
     cost_n_obs = main_plot_n_obs(n_itr=20)
     cost_expo = main_plot_exp_time(n_itr=20)
     cost_dir_unc = main_plot_uncertainty(n_itr=10)

zodipol/estimation/base_calibration.py
@@ -102,7 +102,7 @@ class BaseCalibration:
         """
         pass
 
-    def estimate_polarizance(self, images: u.Quantity, kernel_size=None, **kwargs) -> None:
+    def estimate_polarizance(self, images: u.Quantity, kernel_size=None, star_pixels=None, **kwargs) -> None:
         """
         Estimate the polarization of every pixel.
         """
@@ -113,7 +113,8 @@ class BaseCalibration:
         stokes_tag = np.einsum('...ij,...jk->...ik', stokes, mueller)
 
         # WLS
-        star_pixels = np.stack([o.star_pixels for o in self.obs], axis=-1)
+        if star_pixels is None:
+            star_pixels = np.stack([o.star_pixels for o in self.obs], axis=-1)
         stokes_tag = np.einsum('ij...,ij->ij...', stokes_tag, ~star_pixels)
         intensity = np.einsum('i...j,ij->i...j', intensity, ~star_pixels)
 
@@ -135,7 +136,8 @@ class BaseCalibration:
         p_est = p_est.repeat(4, axis=-1)
         self.p = p_est
 
-    def estimate_birefringence(self, images: u.Quantity, kernel_size: int = None, normalize_eigs: bool = False, **kwargs) -> None:
+    def estimate_birefringence(self, images: u.Quantity, kernel_size: int = None, normalize_eigs: bool = False,
+                               star_pixels=None, **kwargs) -> None:
         """
         Estimate the birefringence of every pixel.
         """
@@ -147,7 +149,8 @@ class BaseCalibration:
         stokes = np.stack([o.to_numpy(ndims=3) for o in self.obs], axis=-1).value
 
         # WLS
-        star_pixels = np.stack([o.star_pixels for o in self.obs], axis=-1)
+        if star_pixels is None:
+            star_pixels = np.stack([o.star_pixels for o in self.obs], axis=-1)
         stokes = np.einsum('i...j,ij->i...j', stokes, ~star_pixels)
         intensity = np.einsum('ij...,ij->ij...', intensity, ~star_pixels)
 
@@ -173,12 +176,13 @@ class BaseCalibration:
         if kernel_size is not None:
             biref_elems = self._biref_smooth_kernel(biref_elems, kernel_size, resolution=self.parser["resolution"])
 
-        if normalize_eigs:
-            biref_elems = self._biref_normalize_eigs(biref_elems)
-            biref_elems /= np.clip(np.abs(biref_elems).max(), 1, None)
-
         biref = np.stack((np.stack((biref_elems[..., 0], biref_elems[..., 1]), axis=-1),
                           np.stack((biref_elems[..., 1], biref_elems[..., 2]), axis=-1)), axis=-1)
+
+        if normalize_eigs:
+            biref = self._biref_normalize_eigs(biref)
+            biref_elems /= np.clip(np.abs(biref_elems).max(), 1, None)
+
         biref = np.clip(biref, -1, 1)
 
         # set necessary values of biref
@@ -188,14 +192,12 @@ class BaseCalibration:
 
     @staticmethod
     def _biref_normalize_eigs(biref_elems):
-        ac_diff = np.clip(biref_elems[:, 0] + biref_elems[:, 2], 0, 2) - (biref_elems[:, 0] + biref_elems[:, 2])
-        biref_elems[:, 0] += ac_diff / 2
-        biref_elems[:, 2] += ac_diff / 2
+        W, V = np.linalg.eig(biref_elems)
+        biref_eigs = W / W.max(axis=1, keepdims=True)
+        biref_res = np.einsum('...ij,...j,...kj->...ik', V, biref_eigs, V)
 
-        # b2_max = abs(2 - biref_elems[:, 0] - biref_elems[:, 2])
-        # biref_elems[:, 1] = np.clip(biref_elems[:, 1], -b2_max, b2_max)
-        biref_elems = np.nan_to_num(biref_elems, nan=0)
-        return biref_elems
+        biref_res = np.nan_to_num(biref_res, nan=0)
+        return biref_res
 
     @staticmethod
     def _biref_smooth_kernel(biref: np.ndarray, kernel_size: int, resolution):

zodipol/estimation/self_calibration.py
@@ -40,13 +40,13 @@ class SelfCalibration(BaseCalibration):
         self.estimate_birefringence(images, **kwargs)
 
     def estimate_polarizance(self, images, **kwargs):
-        super().estimate_polarizance(images)
+        super().estimate_polarizance(images, **kwargs)
         max_p = (kwargs['max_p'] if 'max_p' in kwargs else 1)
         self.p = self.p - np.quantile(self.p[~self.nan_mask], 0.95) + max_p
         self.p = np.clip(self.p, 0, 1)
 
     def estimate_birefringence(self, images, kernel_size: int = None, normalize_eigs: bool = False, **kwargs):
-        super().estimate_birefringence(images, kernel_size=kernel_size, normalize_eigs=normalize_eigs)
+        super().estimate_birefringence(images, kernel_size=kernel_size, normalize_eigs=normalize_eigs, **kwargs)
 
     def get_nan_mask(self, images):
         """