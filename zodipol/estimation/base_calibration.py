Rotation bug fix - slow

zodipol/estimation/base_calibration.py
@@ -190,16 +190,15 @@ class BaseCalibration:
         F_k_inv = np.linalg.pinv(F_K)
         biref_elems = np.einsum('...ij,...j->...i', F_k_inv, N_I)
 
-        # smooth biref
-        if kernel_size is not None:
-            biref_elems = self._biref_smooth_kernel(biref_elems, kernel_size, resolution=self.parser["resolution"])
-
         biref = np.stack((np.stack((biref_elems[..., 0], biref_elems[..., 1]), axis=-1),
                           np.stack((biref_elems[..., 1], biref_elems[..., 2]), axis=-1)), axis=-1)
 
         if normalize_eigs:
             biref = self._biref_normalize_eigs(biref)
-            biref_elems /= np.clip(np.abs(biref_elems).max(), 1, None)
+
+        # smooth biref
+        if kernel_size is not None:
+            biref = self._biref_smooth_kernel(biref, kernel_size, resolution=self.parser["resolution"])
 
         biref = np.clip(biref, -1, 1)
 
@@ -230,7 +229,7 @@ class BaseCalibration:
         :param kernel_size: The size of the kernel used for the uniform filter.
         :param resolution: The resolution of the image.
         """
-        biref_resh = biref.reshape(resolution + list(biref.shape[1:]))
+        biref_resh = biref.reshape(resolution + [np.prod(biref.shape[1:])])
         biref_smooth = np.stack([uniform_filter(biref_resh[..., ii], size=kernel_size, mode='nearest') for ii in
                                  range(biref_resh.shape[-1])], axis=-1)
         biref = biref_smooth.reshape(biref.shape)

zodipol/estimation/self_calibration.py
@@ -51,8 +51,8 @@ class SelfCalibration(BaseCalibration):
         self.estimate_birefringence(images, **kwargs)
 
     def _pose_constraints_p(self, p):
-        rho = p / self.init['p'][:, 0, None]
-        p /= np.quantile(rho, 0.99)
+        rho = p[self.nan_mask] / self.init['p'][self.nan_mask, 0, None]
+        p /= np.quantile(rho, 0.95)
         p = np.clip(p, 0, 1)
         return p
 
@@ -99,8 +99,7 @@ class SelfCalibration(BaseCalibration):
         interp_images_res = self.aligned_images
         p = align_images(self.zodipol, self.parser, self.p.repeat(self.nobs, axis=-1), rotation_list, invert=True)[..., None, :].repeat(self.parser["n_polarization_ang"], axis=-2)
         eta = align_images(self.zodipol, self.parser, self.eta[:, None, None].repeat(self.nobs, axis=-1), rotation_list, invert=True) + self.parser["polarization_angle"][:, None]
-        mueller = self.biref
-        mueller_rot = mueller[..., None].repeat(self.nobs, axis=-1)
+        mueller_rot = self.biref[..., None].repeat(self.nobs, axis=-1)
         mueller_rot = align_images(self.zodipol, self.parser, mueller_rot, rotation_list, invert=True)
         rotation_mat = get_rotation_mueller_matrix(np.deg2rad(rotation_list))[None, ...].repeat(images.shape[0], axis=0)
         I_est, Q_est, U_est = self.estimate_IQU(interp_images_res, rotation_mat, p, eta, mueller_rot)
@@ -108,8 +107,7 @@ class SelfCalibration(BaseCalibration):
         obs_est_rot = [self.realign_observation(obs_est, roll) for roll in rotation_list]
         return obs_est_rot
 
-    @staticmethod
-    def estimate_IQU(intensity, rotation_mat, polarizance, angles, mueller):
+    def estimate_IQU(self, intensity, rotation_mat, polarizance, angles, mueller):
         """
         Calculate the Stokes parameters of the signal.
         :param intensity: The intensity of the signal.
@@ -135,7 +133,7 @@ class SelfCalibration(BaseCalibration):
         :param roll: roll angle
         :return: realigned observation
         """
-        stokes_interp = get_rotated_image(self.zodipol, self.parser, obs.to_numpy(ndims=3), roll)
+        stokes_interp = get_rotated_image(self.zodipol, self.parser, obs.to_numpy(ndims=3), 0, roll)
         obs_new = Observation(stokes_interp[..., 0], stokes_interp[..., 1], stokes_interp[..., 2], theta=obs.theta,
                               phi=obs.phi, roll=0)
         return obs_new.change_roll(np.deg2rad(roll))

zodipol/zodipol/generate.py
@@ -48,7 +48,7 @@ def get_initial_parameters(obs: List[Observation], parser: ArgParser, zodipol: Z
         direction_uncertainty = parser["direction_uncertainty"]
     obs = [o.add_radial_blur(direction_uncertainty, list(parser["resolution"])) for o in obs]
     obs = [o.add_direction_uncertainty(parser["fov"], parser["resolution"], direction_uncertainty) for o in obs]
-    [o.dilate_star_pixels(1 + (direction_uncertainty / zodipol.fov * np.min(parser['resolution'])).value.astype(int), parser["resolution"]) for o in obs]
+    obs = [o.dilate_star_pixels(1 + (direction_uncertainty / zodipol.fov * np.min(parser['resolution'])).value.astype(int), parser["resolution"]) for o in obs]
 
     if mode == 'linear':
         delta_val, phi_val = np.pi / 8, np.pi / 6
@@ -96,8 +96,7 @@ def get_initial_parameters(obs: List[Observation], parser: ArgParser, zodipol: Z
     polarization_angle_spatial_diff = np.zeros_like(polarizance)
     polarization_angle_real = polarization_angle[None, None, :] + polarization_angle_spatial_diff.flatten()[:, None, None]
 
-    obs_orig = [zodipol.make_camera_images(o, polarizance_real, polarization_angle_real, n_realizations=parser["n_realizations"],
-                                           add_noise=True) for o in obs_biref]
+    obs_orig = [zodipol.make_camera_images(o, polarizance_real, polarization_angle_real, n_realizations=parser["n_realizations"], add_noise=False) for o in obs_biref]
     images_orig = zodipol.post_process_images(np.stack(obs_orig, axis=-1))
     return obs, images_orig, polarizance_real.reshape(-1, parser["n_polarization_ang"]), polarization_angle_spatial_diff.reshape((-1)), mueller_truth
 

zodipol/zodipol/observation.py
@@ -128,6 +128,10 @@ class Observation:
         :param direction_uncertainty: amount of direction uncertainty of the observation (degrees)
         :return: new Observation object
         """
+        if direction_uncertainty.value < 0:
+            raise ValueError('Circular motion blur must be positive')
+        elif direction_uncertainty.value == 0:
+            return self.copy()
         pixel_size = fov / resolution
         pixels_uncertainty = (direction_uncertainty / pixel_size).value
         pixels_uncertainty = np.concatenate((pixels_uncertainty, [1]))
@@ -180,6 +184,8 @@ class Observation:
         :param n_pixels: number of pixels to dilate
         :param resolution: resolution of the observation
         """
-        px = self.star_pixels.reshape(resolution)
+        self_copy = self.copy()
+        px = self_copy.star_pixels.reshape(resolution)
         px_dilate = dilation(px, disk(n_pixels))
-        self.star_pixels = px_dilate.reshape(self.star_pixels.shape)
+        self_copy.star_pixels = px_dilate.reshape(self_copy.star_pixels.shape)
+        return self_copy